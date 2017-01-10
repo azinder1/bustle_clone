cbfc7a11d5cbd55c893d58a6cf353d30d94d66b5
Adam Zinder and Erica Nafziger <adam@>
2017-01-10 12:02:21 -0800

    create some add post functionality

--- app/routes/add.js
+++ app/routes/add.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    savePost() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+        image: this.get('image'),
+        category: this.get('category')
+      };
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('admin')
+    }
+  }
 });
